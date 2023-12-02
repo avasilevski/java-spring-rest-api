70eaa3b5bee81984e9347121cdd3dec0cfc685a4
avasilevski <vasilevski85@gmail.com>
2023-12-02 11:57:57 -0500

    initial project files

---
+++ src/main/java/com/aleksandarvasilevski/javaspringrestapi/JavaSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandarvasilevski.javaspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksandarvasilevski/javaspringrestapi/JavaSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandarvasilevski.javaspringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
