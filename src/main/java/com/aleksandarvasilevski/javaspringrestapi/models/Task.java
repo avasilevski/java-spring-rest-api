2fff32690c8680b3323e8e08bc9a4356b0095216
avasilevski <vasilevski85@gmail.com>
2023-12-09 13:31:37 -0500

    added missing annotations in task entity

--- src/main/java/com/aleksandarvasilevski/javaspringrestapi/models/Task.java
+++ src/main/java/com/aleksandarvasilevski/javaspringrestapi/models/Task.java
@@ -1,7 +1,14 @@
 package com.aleksandarvasilevski.javaspringrestapi.models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
