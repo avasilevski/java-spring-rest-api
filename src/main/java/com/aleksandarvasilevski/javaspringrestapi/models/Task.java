941581f0e530e3b18b77d9bf3a408e79fe1f8d7d
avasilevski <vasilevski85@gmail.com>
2023-12-11 15:02:53 -0500

    added example test for task entity, temporary removed duedate property

--- src/main/java/com/aleksandarvasilevski/javaspringrestapi/models/Task.java
+++ src/main/java/com/aleksandarvasilevski/javaspringrestapi/models/Task.java
@@ -4,6 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 public class Task {
@@ -12,7 +13,6 @@ public class Task {
     private long id;
     private String title;
     private String description;
-    private LocalDateTime dueDate;
     private boolean completed;
 
     // Constructors, getters, setters and other methods
@@ -21,11 +21,10 @@ public class Task {
         // default constructor
     }
 
-    public Task(Long id, String title, String description, boolean completed){
+    public Task(long id, String title, String description, boolean completed){
         this.id = id;
         this.title = title;
         this.description = description;
-        this.dueDate = LocalDateTime.now();
         this.completed = completed;
     }
 
@@ -39,10 +38,6 @@ public class Task {
 
     public String getDescription(){
         return this.description;
-    }
-
-    public LocalDateTime getDueDate(){
-        return this.dueDate;
     }
 
     public boolean isCompleted(){
@@ -59,10 +54,6 @@ public class Task {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public void setDueDate(LocalDateTime dueDate) {
-        this.dueDate = dueDate;
     }
 
     public void setCompleted(boolean completed) {
---
+++ src/test/java/com/aleksandarvasilevski/javaspringrestapi/TaskJsonTest.java
@@ -0,0 +1,54 @@
+package com.aleksandarvasilevski.javaspringrestapi;
+
+import com.aleksandarvasilevski.javaspringrestapi.models.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class TaskJsonTest {
+
+    @Autowired
+    private JacksonTester<Task> json;
+
+    @Test
+    void taskSerializationTest() throws IOException {
+        Task task = new Task(1L, "Foo", "Foo something...", false);
+        assertThat(json.write(task)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(task)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(task)).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(1);
+        assertThat(json.write(task)).hasJsonPathStringValue("@.title");
+        assertThat(json.write(task)).extractingJsonPathStringValue("@.title")
+                .isEqualTo("Foo");
+        assertThat(json.write(task)).hasJsonPathStringValue("@.description");
+        assertThat(json.write(task)).extractingJsonPathStringValue("@.description")
+                .isEqualTo("Foo something...");
+        assertThat(json.write(task)).hasJsonPathBooleanValue("@.completed");
+        assertThat(json.write(task)).extractingJsonPathBooleanValue("@.completed")
+                .isEqualTo(false);
+    }
+
+    @Test
+    void taskDeserializationTest() throws IOException {
+        String expected = """
+           {
+              "id":1,
+              "title": "Foo",
+              "description": "Foo something...",
+              "completed": false
+           }
+           """;
+        assertThat(json.parse(expected)).isEqualToComparingFieldByField(new Task(1L, "Foo", "Foo something...", false));
+        assertThat(json.parseObject(expected).getId()).isEqualTo(1);
+        assertThat(json.parseObject(expected).getTitle()).isEqualTo("Foo");
+        assertThat(json.parseObject(expected).getDescription()).isEqualTo("Foo something...");
+        assertThat(json.parseObject(expected).isCompleted()).isEqualTo(false);
+    }
+}
+
